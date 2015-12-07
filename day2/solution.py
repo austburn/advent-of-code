b4dc2e1c314a9895a3bf2a79f623cdfb06d3096c
austburn <austburn@gmail.com>
2015-12-07 14:34:20 -0500

    Add comments for day 2.

--- day2/solution.py
+++ day2/solution.py
@@ -4,8 +4,12 @@ dimensions = f.readlines()
 paper_needed = 0
 
 for dimension in dimensions:
+    # Translate '2x6x4\n' => [2, 6, 4]
     int_dimensions = map(lambda x: int(x), dimension.rstrip('\n').split('x'))
+    # Sort it so we know what the smallest side is
     int_dimensions.sort()
+    # Honestly, just some lambda experimentation
+    # We now have [2, 4, 6], so we need to add short*medium (smallest side) to the total surface area
     paper_needed += (lambda short, medium, lng: short*medium + 2*short*medium + 2*medium*lng + 2*short*lng)(*int_dimensions)
 
 print(paper_needed)
